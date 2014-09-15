c6b62f1fb8ce7ea1805c87f141b4d96ba339bfd6
Sebastian Bredehöft <sebastian.bredehoeft@comsulting.de>
2014-09-15 15:58:07 +0200

    ADDED: setup.py

---
+++ setup.py
@@ -0,0 +1,48 @@
+import os
+from setuptools import setup
+
+README = """
+Django REST Swagger
+
+An API documentation generator for Swagger UI and Django REST Framework version 2.3+
+
+Installation
+From pip:
+
+pip install django-rest-swagger
+
+Docs & details @
+https://github.com/marcgibbons/django-rest-swagger
+"""
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-rest-hal',
+    version='0.0.1',
+    packages=['django_rest_hal'],
+    # package_data={'django_rest_hal': ['django_rest_hal/*']},
+    # include_package_data=True,
+    license='FreeBSD License',
+    description='HAL Implementation for Django REST Framework 2.3+',
+    long_description=README,
+    install_requires=[
+        'django>=1.6',
+        'djangorestframework>=2.3.5',
+    ],
+
+    url='http://github.com/seebass',
+    author='Sebastian Bredehöft',
+    author_email='bredehoeft.sebastian@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
