fd1607a2aa4e2a760ac1227edfed91fd492c63ca
seebass <bredehoeft.sebastian@gmail.com>
2014-09-18 09:23:08 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-rest-hal',
-    version='0.0.1',
+    version='0.0.2',
     packages=['django_rest_hal'],
     # package_data={'django_rest_hal': ['django_rest_hal/*']},
     # include_package_data=True,
